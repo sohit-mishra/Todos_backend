a168c495b6b086c0cdd6d109a4967737f689ce15
Sohit Mishra <147324802+sohit-mishra@users.noreply.github.com>
2024-09-06 11:36:35 +0530

    Update todos.js

--- models/todos.js
+++ models/todos.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-    title: { type: String, required: true }
+    title: { type: String, required: true },
     description: { type: String, required: true }
 }, { timestamps: true })
 
