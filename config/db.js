d75b4fc14946fa42873c44fc5d23fb2c6dc391cc
Sohit Mishra <147324802+sohit-mishra@users.noreply.github.com>
2024-09-21 20:11:50 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const URI = process.env.URI
+const URI = process.env.URI;
+console.log('MongoDB URI:', URI); // Check if the URI is logged correctly
 
-const connectToDatabase = async()=>{
-    try {
-        await mongoose.connect(URI) ;
-        console.log("Database is connect");
-    } catch (error) {
-        console.log(error); 
-    }
-}
-
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Database is connected");
+  } catch (error) {
+    console.log("Error connecting to the database:", error);
+  }
+};
 
 module.exports = connectToDatabase;
